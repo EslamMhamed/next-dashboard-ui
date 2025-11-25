5a6d28b9ebd1b53dbdc1cf75aa620965e888e26e
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-11-25 21:51:12 +0200

    admin Page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,8 +1,22 @@
+import UserCard from "@/components/UserCard";
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
-  )
+    <div className="flex flex-col md:flex-row p-4 gap-4">
+      {/* LEFT */}
+      <div className="w-2/3">
+        <div className="flex  gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-1/3">R</div>
+    </div>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
