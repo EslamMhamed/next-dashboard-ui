790e2c38d1b915a06cd8fd2cdeeb91aafd3a648a
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-11-25 21:42:23 +0200

    update layout dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      {/* LEFT */}
+      {/* RIGHT */}
       <div className=" w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll ">
         <Navbar />
         {children}
