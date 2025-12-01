17d60fadf017a4edd6101880077840c066b09509
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-01 22:42:00 +0200

    create Finance component

---
+++ components/FinanceChart.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Image from "next/image";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+
+const data = [
+  {
+    name: 'Jan',
+    income: 4000,
+    expense: 2400,
+  },
+  {
+    name: 'Feb',
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: 'Mar',
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: 'Apr',
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: 'May',
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: 'Jun',
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: 'Jul',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Aug',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Sep',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Oct',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Nov',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Dec',
+    income: 3490,
+    expense: 4300,
+  },
+];
+
+function FinanceChart() {
+  return (
+    <div className="bg-white rounded-lg h-full p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Finance</h1>
+        <Image src="/moreDark.png" alt="moredark" width={20} height={20} />
+      </div>
+      <LineChart
+      width='100%' height='90%'
+      responsive
+      data={data}
+      
+    >
+      <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+      <XAxis dataKey="name" tick={{fill:'#d1d5db'}} axisLine={false} tickLine={false} tickMargin={10} />
+      <YAxis width="auto" tick={{fill:'#d1d5db'}} axisLine={false} tickLine={false} tickMargin={20}  />
+      <Tooltip />
+      <Legend align="center" verticalAlign="top" wrapperStyle={{paddingTop: '10px', paddingBottom: '30px'}} />
+      <Line type="monotone" dataKey="income" stroke="#c3ebfa" strokeWidth={5} />
+      <Line type="monotone" dataKey="expense" stroke="#cfceff" strokeWidth={5} />
+    </LineChart>
+    </div>
+  );
+}
+
+export default FinanceChart;
