2068ca645e04942f5a9830bd917c2248192042f0
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-01 23:14:29 +0200

    update admin page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalender from "@/components/EventCalender";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
@@ -35,7 +36,9 @@ function AdminPage() {
       </div>
 
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">R</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8" >
+        <EventCalender />
+      </div>
     </div>
   );
 }
