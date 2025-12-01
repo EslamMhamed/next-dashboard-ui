2d64caff7b5762c8ca540b45e4e8799b7f091efc
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-01 22:42:16 +0200

    update admin page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 function AdminPage() {
@@ -27,7 +28,9 @@ function AdminPage() {
         </div>
 
         {/* BOTTOM CHARTS */}
-        <div className="flex flex-col lg:flex-row gap-4 "></div>
+        <div className="w-full h-[500px] ">
+          <FinanceChart />
+        </div>
 
       </div>
 
