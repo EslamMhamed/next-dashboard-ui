6d1c7ef738a040adb53c6a19e2a7763195558bdd
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-02 20:16:42 +0200

    create Announcements component

---
+++ components/Announcements.tsx
@@ -0,0 +1,39 @@
+
+function Announcements() {
+  return (
+    <div className="p-4 rounded-md bg-white">
+        <div className="flex items-center justify-between">
+            <h1 className="font-semibold text-xl">Announcements</h1>
+            <span className="text-xs text-gray-400">View All</span>
+        </div>
+        <div className="flex flex-col gap-4 mt-4">
+            <div className="bg-lamaSkyLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium ">Lorem ipsum dolor sit amet consectetur, quis.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+
+
+                <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At numquam ad voluptates eius iure quos.</p>
+            </div>
+            <div className="bg-lamaPurpleLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium ">Lorem ipsum dolor sit amet consectetur, quis.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+
+                <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At numquam ad voluptates eius iure quos.</p>
+            </div>
+            <div className="bg-lamaYellowLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium ">Lorem ipsum dolor sit amet consectetur, quis.</h2>
+                    <span className="text-xs text-gray-400 bg-white rounded-md p-1">2025-01-01</span>
+                </div>
+                <p className="text-sm text-gray-400 mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At numquam ad voluptates eius iure quos.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Announcements
