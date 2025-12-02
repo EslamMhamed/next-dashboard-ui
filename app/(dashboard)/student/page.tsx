d11e22d9c0b6221d91d85be862a979540de32570
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-02 20:19:52 +0200

    Finish admin page

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,7 +1,21 @@
+import Announcements from "@/components/Announcements"
+import EventCalender from "@/components/EventCalender"
 
 function StudentPage() {
   return (
-    <div>StudentPage</div>
+    <div className="p-4 flex flex-col gap-4 xl:flex-row">
+      {/* Left */}
+        <div className="w-full xl:w-2/3">
+          <div className="h-full rounded-md bg-white p-4">
+            <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          </div>
+        </div>
+      {/* Right */}
+        <div className="w-full xl:w-1/3 flex flex-col gap-8">
+          <EventCalender />
+          <Announcements />
+        </div>
+    </div>
   )
 }
 
