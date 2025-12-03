32e01b5f845a26f8d8f5724e51b7919dd2d40d38
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-03 15:52:22 +0200

    create results page

---
+++ app/(dashboard)/list/results/page.tsx
@@ -0,0 +1,112 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { resultsData, role } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+export type Assignment = {
+  id: number;
+  subject: string;
+  class: string;
+  teacher: string;
+  student: string;
+  date: string;
+  type: "exam" | "assignment";
+  score: number; 
+};
+
+const columns = [
+  {
+    header: "Subject ",
+    accessor: "subject",
+  },
+  {
+    header: "Student",
+    accessor: "student",
+  },
+  {
+    header: "Score",
+    accessor: "score",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  { header: "Actions", accessor: "actions" },
+];
+
+function ResultsListPage() {
+  const renderRow = (item: Assignment) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="p-4">
+        <h3 className="font-semibold">{item.subject}</h3>
+      </td>
+      <td>{item.student}</td>
+      <td >{item.score}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teacher/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="view" height={16} width={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" alt="view" height={16} width={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+      <div className="flex items-center justify-between  ">
+        <h1 className="hidden md:block text-lg font-semibold">All Results</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="filter" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="filter" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <Image src="/sort.png" alt="filter" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+}
+
+export default ResultsListPage;
