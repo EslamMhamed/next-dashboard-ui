79b23877105b888e18c66859b18048f35d60128c
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-04 13:13:54 +0200

    create TeacherForm component

---
+++ components/form/TeacherForm.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+type TeacherFormProps ={
+    data ?: any ; 
+    type: "create" | "update"
+}
+
+function TeacherForm() {
+  return (
+    <form>
+        input
+    </form>
+  )
+}
+
+export default TeacherForm
