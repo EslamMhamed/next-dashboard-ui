b861a3342a6bc56a842213662f3cc84713bb76fe
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-06 23:41:08 +0200

    update SingleTeacherPage page

--- app/(dashboard)/list/teachers/[id]/page.tsx
+++ app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import FormModal from "@/components/FormModal";
 import Performance from "@/components/Performance";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,23 +25,45 @@ function SingleTeacherPage() {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
+            <div className="flex items-center gap-4">
               <h1 className="text-xl font-semibold">Leonard Snyder</h1>
-              <p className="text-sm text-gray-500">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <FormModal
+                table="teacher"
+                type="create"
+                data={{
+                  id: 1,
+                  username: "deanguerrero",
+                  email: "deanguerrero@gmail.com",
+                  password: "password",
+                  firstName: "Dean",
+                  lastName: "Guerrero",
+                  phone: "+1 234 567 89",
+                  address: "1234 Main St, Anytown, USA",
+                  bloodType: "A+",
+                  dateOfBirth: "2000-01-01",
+                  sex: "male",
+                  img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                }}
+              />
+              </div>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </p>
               <div className="flex justify-between items-center  gap-2 flex-wrap text-xs font-medium ">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 ">
-                  <Image src='/blood.png' width={14} height={14} alt="blood" />
+                  <Image src="/blood.png" width={14} height={14} alt="blood" />
                   <span>A+</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 ">
-                  <Image src='/date.png' width={14} height={14} alt="date" />
+                  <Image src="/date.png" width={14} height={14} alt="date" />
                   <span>May 2025</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 ">
-                  <Image src='/mail.png' width={14} height={14} alt="mail" />
+                  <Image src="/mail.png" width={14} height={14} alt="mail" />
                   <span>mohaeslam22@gamil.com</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2 ">
-                  <Image src='/phone.png' width={14} height={14} alt="phone" />
+                  <Image src="/phone.png" width={14} height={14} alt="phone" />
                   <span className="text-xs">+20 1004430354</span>
                 </div>
               </div>
@@ -50,7 +73,13 @@ function SingleTeacherPage() {
           <div className="flex-1 flex justify-between gap-4 flex-wrap">
             {/* CARD */}
             <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
-              <Image src='/singleAttendance.png' width={24} height={24} alt="signle" className="w-6 h-6" />
+              <Image
+                src="/singleAttendance.png"
+                width={24}
+                height={24}
+                alt="signle"
+                className="w-6 h-6"
+              />
               <div className="">
                 <h1 className="text-xl font-semibold ">90%</h1>
                 <span className="text-gray-400 text-sm ">Attendance</span>
@@ -58,7 +87,13 @@ function SingleTeacherPage() {
             </div>
             {/* CARD */}
             <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
-              <Image src='/singleBranch.png' width={24} height={24} alt="signle" className="w-6 h-6" />
+              <Image
+                src="/singleBranch.png"
+                width={24}
+                height={24}
+                alt="signle"
+                className="w-6 h-6"
+              />
               <div className="">
                 <h1 className="text-xl font-semibold ">2</h1>
                 <span className="text-gray-400 text-sm ">Branches</span>
@@ -66,7 +101,13 @@ function SingleTeacherPage() {
             </div>
             {/* CARD */}
             <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
-              <Image src='/singleLesson.png' width={24} height={24} alt="signle" className="w-6 h-6" />
+              <Image
+                src="/singleLesson.png"
+                width={24}
+                height={24}
+                alt="signle"
+                className="w-6 h-6"
+              />
               <div className="">
                 <h1 className="text-xl font-semibold ">6</h1>
                 <span className="text-gray-400 text-sm ">Lessons</span>
@@ -74,7 +115,13 @@ function SingleTeacherPage() {
             </div>
             {/* CARD */}
             <div className="bg-white w-full p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%] ">
-              <Image src='/singleClass.png' width={24} height={24} alt="signle" className="w-6 h-6" />
+              <Image
+                src="/singleClass.png"
+                width={24}
+                height={24}
+                alt="signle"
+                className="w-6 h-6"
+              />
               <div className="">
                 <h1 className="text-xl font-semibold ">6</h1>
                 <span className="text-gray-400 text-sm ">Classes</span>
@@ -90,18 +137,28 @@ function SingleTeacherPage() {
       </div>
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
-      <div className="bg-white p-4 rounded-md">
-        <h1 className="text-xl font-semibold">Shortcuts</h1>
-        <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-          <Link href='/' className="p-3 rounded-md bg-lamaSkyLight" >Teacher&apos;s Classes</Link>
-          <Link href='/' className="p-3 rounded-md bg-lamaPurpleLight" >Teacher&apos;s Students</Link>
-          <Link href='/' className="p-3 rounded-md bg-lamaYellowLight" >Teacher&apos;s Lessons</Link>
-          <Link href='/' className="p-3 rounded-md bg-pink-50" >Teacher&apos;s Exams</Link>
-          <Link href='/' className="p-3 rounded-md bg-lamaSkyLight" >Teacher&apos;s Assignments</Link>
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">
+              Teacher&apos;s Classes
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-lamaPurpleLight">
+              Teacher&apos;s Students
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-lamaYellowLight">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-pink-50">
+              Teacher&apos;s Exams
+            </Link>
+            <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
         </div>
-      </div>
-      <Performance />
-      <Announcements />
+        <Performance />
+        <Announcements />
       </div>
     </div>
   );
